1c6ffc74a7cd677d30dcba761c7e29b7c148a25f
A-Evann <ambroisinevann@gmail.com>
2020-06-11 16:33:18 +0200

    Début de la page d'accueil

--- vmvdc/vmvdc/resources/views/layout.blade.php
+++ vmvdc/vmvdc/resources/views/layout.blade.php
@@ -11,6 +11,8 @@
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+        <link rel="stylesheet" href="../public/css/style.css">
     </head>
     <body>
         <div class="flex-center position-ref full-height">
@@ -29,9 +31,6 @@
 
                 @yield ('contenu')
 
-
-
-                
             </div>
         </div>
     </body>
--- vmvdc/vmvdc/resources/views/welcome.blade.php
+++ vmvdc/vmvdc/resources/views/welcome.blade.php
@@ -2,8 +2,19 @@
 
 @section('contenu')
 
-                <div class="title m-b-md">
-                    <h1>Vis ma vie de chercheur</h1>
-                </div>
+<div class="d-flex bd-highlight">
+  <div class="p-2 flex-grow-1 bd-highlight align-middle" style="background-color: aqua;"><a href="/">Logo</a></div>
+  <div class="p-2 bd-highlight" style="background-color: mediumslateblue;"><a href="#">Se connecter</a></div>
+  <div class="p-2 bd-highlight" style="background-color: navajowhite;"><a href="/inscription">Se créer un compte</a></div>
+</div>
+
+<img src="C:\Users\Evann\Pictures\Screenshots\image.png" class="img-fluid" alt="Responsive image">
+
+<img src="C:\Users\Evann\Pictures\Screenshots\image.png" alt="Responsive image">
+
+
+<div class="card-title">
+    <h1>Vis ma vie de chercheur</h1>
+</div>
 
 @endsection
