9c81646ed107813195278f5dbe579d414f9b2ffc
A-Evann <ambroisinevann@gmail.com>
2020-06-11 10:47:34 +0200

    ajout de colonne

--- vmvdc/vmvdc/resources/views/welcome.blade.php
+++ vmvdc/vmvdc/resources/views/welcome.blade.php
@@ -2,6 +2,28 @@
 
 @section('contenu')
 
+                <div class="container">
+                <div class="row">
+                    <div class="col">
+                    1 of 2
+                    </div>
+                    <div class="col">
+                    2 of 2
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                    1 of 3
+                    </div>
+                    <div class="col">
+                    2 of 3
+                    </div>
+                    <div class="col">
+                    3 of 3
+                    </div>
+                </div>
+                </div>
+
                 <div class="title m-b-md">
                     Vis ma vie de chercheur
                 </div>
