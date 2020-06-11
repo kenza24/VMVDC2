5249095dcef6418c1b0d12a968d3b21bc62a6309
A-Evann <ambroisinevann@gmail.com>
2020-06-11 14:42:45 +0200

    Suppression de la barre de navigation et des liens dans le layout et dans la page d'acceuil

--- vmvdc/vmvdc/resources/views/welcome.blade.php
+++ vmvdc/vmvdc/resources/views/welcome.blade.php
@@ -3,14 +3,7 @@
 @section('contenu')
 
                 <div class="title m-b-md">
-                    Vis ma vie de chercheur
-                </div>
-                
-                <div class="links">
-                  <a href="#">Accueil</a>
-                  <a href="#">Doctorants</a>
-                  <a href="https://laravel-news.com">Administrateur</a>
-                  <a href="https://forge.laravel.com">Enseignant</a>
+                    <h1>Vis ma vie de chercheur</h1>
                 </div>
 
 @endsection
