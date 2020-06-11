14f2622d309f203d637e15b558e234b1024563aa
Kenza Zerouali <49944467+kenza24@users.noreply.github.com>
2020-06-11 12:17:13 +0200

    Update welcome.blade.php

--- vmvdc/vmvdc/resources/views/welcome.blade.php
+++ vmvdc/vmvdc/resources/views/welcome.blade.php
@@ -5,5 +5,12 @@
                 <div class="title m-b-md">
                     Vis ma vie de chercheur
                 </div>
+                
+                <div class="links">
+                  <a href="#">Accueil</a>
+                  <a href="#">Doctorants</a>
+                  <a href="https://laravel-news.com">Administrateur</a>
+                  <a href="https://forge.laravel.com">Enseignant</a>
+                </div>
 
 @endsection
