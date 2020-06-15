c28e6aafa8c67f7bc460859b00e29211748cc0d2
kenza24 <kenzazerouali00@gmail.com>
2020-06-15 11:47:02 +0200

    Update welcome.blade.php

--- vmvdc/vmvdc/resources/views/welcome.blade.php
+++ vmvdc/vmvdc/resources/views/welcome.blade.php
@@ -2,12 +2,11 @@
 
 @section('contenu')
 
+<!-- En-tete -->
 <div class="container-fluid">
-
-  <!-- Columns are always 50% wide, on mobile and desktop -->
   <div class="row" style="background-color: #11385b;">
     <a href="">
-      <img src="content/Logo-IBPS.png" alt="Logo-IBPS" class="float-left" style="height: 150px;">
+      <img src="content/ibps-logo.jpg" alt="Logo-IBPS" class="float-left" style="height: 150px;">
     </a>
     <div class="col-md text-center text-wrap text-break content_center" style="color: white; height:100px; margin-top: 50px;">
       <h1 style="vertical-align: middle;">Vis ma vie de chercheur</h1>
@@ -21,15 +20,18 @@
       <h5>Se connecter</h5>
     </a>-->
   </div>
-  
 </div>
 
+<!--Bandeau-->
 <div class="text-center mb-5">
   <img src="content/bandeau.png" class="img-fluid" alt="Responsive image" style="height: 300px;">
 </div>
 
+<!--Cartes explicatives-->
 <div class="container-fluid mt-5 mb-5">
   <div class="row d-flex justify-content-around">
+
+  <!--Carte 1-->
     <div class="card col-lg-5">
       <div class="card-body">
         <h5 class="card-title">Descriptif du projet</h5>
@@ -39,6 +41,7 @@
       </div>
     </div>
 
+    <!--Carte 2-->
     <div class="card col-lg-5">
       <div class="card-body">
         <h5 class="card-title">Démarche pour y participer</h5>
@@ -47,9 +50,11 @@
         </p>
       </div>
     </div>
+
   </div>
 </div>
 
+<!--Financement-->
 <div class="container-fluid mt-5 mb-5">
   <div class="row d-flex justify-content-around mt-5 mb-5">
     <div class="col-md-2 d-flex justify-content-around mt-5 mb-5" style="color: white;">
@@ -57,15 +62,32 @@
     </div>
   </div>
   <div class="row d-flex justify-content-around mt-5 mb-5">
-    <a href="https://www.paris.fr/">
-      <img src="content/ville-de-paris.jpeg" alt="Sponsor1" class="img-thumbnail">
-    </a>
-    <a href="http://www.sorbonne-universite.fr/">
-      <img src="content/sorbonne-universite.png" alt="Sponsor2" class="img-thumbnail">
-    </a>
+    <div class="col-md-2">
+      <a href="https://www.paris.fr/" style="background-color: white;">
+        <img src="content/ville-de-paris.png" alt="Sponsor1" class="img-thumbnail" style="width: 100%">
+      </a>
+    </div>
+    <div class="col-md-2">
+      <a href="http://www.sorbonne-universite.fr/">
+        <img src="content/su-logo.jpg" alt="Sponsor2" class="img-thumbnail" style="width: 100%;">
+      </a>
+    </div>
+    <div class="col-md-2">
+      <a href="https://www.iledefrance.fr/">
+        <img src="content/idf-logo.jpg" alt="Sponsor3" class="img-thumbnail" style="width: 100%;">
+      </a>
+    </div>
+    <div class="col-md-2">
+      <a href="http://sciences.sorbonne-universite.fr/ufr-sciences-de-la-vie">
+        <img src="content/UFR-science-de-la-vie.jpg" alt="Sponsor4" class="img-thumbnail" style="width: 100%;">
+      </a>
+    </div>
+
   </div>
 </div>
 
+
+<!--Pied de page-->
 <div class="container-fluid mt-5" style="background-color: #11385b;">
   <br>
   <br>
