383b56dc81cbf9820b91405fbcfe23b98bffddf1
kenza24 <kenzazerouali00@gmail.com>
2020-06-18 13:09:45 +0200

    modif page enseignant

--- vmvdc/vmvdc/resources/views/enseignants.blade.php
+++ vmvdc/vmvdc/resources/views/enseignants.blade.php
@@ -12,6 +12,9 @@
       <div class="col-md text-center text-wrap text-break content_center" style="color: white; height:50px; margin-top: 25px; margin-left: 100px;">
         <h1 style="vertical-align: middle;">Vis ma vie de chercheur</h1>
       </div>
+      <form action={{route('deconnexione')}}>
+       <button type="submit" class="btn btn-primary btn-block">Se déconnecter</button>
+     </form>
     </div>
   </div>
 </div>
@@ -19,7 +22,10 @@
   <br>
   <br>
   <div class="col-md text-center text-wrap text-break content_center" style="color: white;font-style: oblique; font-family: Georgia, serif;">
-    <h1> Blabla Untel </h1>
+    <h1><?php if (isset($_SESSION['id'])): ?>
+                  <?= $_SESSION['id'] ?>
+                <?php endif ?>
+    </h1>
   </div>
 
   <div  class="container">
@@ -31,16 +37,34 @@
       <br>
       <br>
 
-      <dl class="row">
-        <dt class="col-xl-5" style="margin-left:30px;">Votre email : <small> test@gmail.com </small> </dt>
-        <dd class="col-m-3"> </dd>
-      </dl>
+    <!--<dl class="row">-->
+        <div class="shadow-lg p-3 mb-5 bg-black rounded" style="width: 1000px; margin-left:55px">
+          <div class="row align-items-center">
+          <div class="col-4">
+            <dt class="col-xl-5" style="margin-left:30px;">Votre email : <?php if (isset($_SESSION['email'])): ?>
+                                                                              <?= $_SESSION['email'] ?>
+                                                                            <?php endif ?> </dt>
+          </div>
+          <div class="col-4">
+            <dt class="col-xl-5" style="margin-left:30px;">Votre nom :</dt>
+          </div>
+          <div class="col-4">
+            <dt class="col-xl-5" style="margin-left:30px;">Votre prénom </dt>
+          </div>
+        </div>
+
+
+        </div>
+      <!--</dl>-->
+
+
+
       <div class="d-flex flex-row">
+
+
       <div class="p-2">
-        <button type="button" class="btn btn-xs btn-secondary" >Modifier mon adresse mail</button>
+        <button type="button" class="btn btn-xs btn-secondary" style="margin-left:400px; margin-top:0px">Modifier mes informations</button>
       </div>
-      <div class="p-2">
-        <button type="button" class="btn btn-xs btn-secondary" style="margin-left:400px; margin-top:1px">Modifier mon mot de passe</button>
       </div>
     </div>
 <!--Fin container-->
@@ -55,10 +79,10 @@
         <div class="p-2">
         <div class="card" style="border: none; border-radius: 0; background-color: #f5f5f5; margin:0;">
          <div class="card flex-md-row mb-4 shadow-sm h-md-250">
-            <div class="card-body d-flex flex-column align-items-start">
+            <div class="card-body d-flex flex-column align-items-start" style="width:250px;">
                <strong class="d-inline-block mb-2 text-secondary">Mes sessions</strong>
                <h6 class="mb-0">
-                  Vous pouvez retrouver un récapitulatif de vos sessions a venir ! blalnvsdnlnsdlnsdlnldnlnflknlknlknlknlknln
+                  Vous pouvez retrouver un récapitulatif de vos sessions a venir !
                </h6>
                <br>
                <br>
@@ -74,7 +98,7 @@
               <div class="card-body d-flex flex-column align-items-start" style="margin-left:1px; width:650px; height: 255px">
                 <strong class="d-inline-block mb-2 text-secondary">Mes documents</strong>
                  <h6 class="mb-0">
-                    lorem ipsblalnvsdnlnsdlnsdlnldnlnflknlknlknlknlknln
+
                  </h6>
                </div>
               </div>
