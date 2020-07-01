31294f9d131e2e57f7086301333bbd322b212756
A-Evann <ambroisinevann@gmail.com>
2020-07-01 16:37:48 +0200

    selection des classes pre-inscrite par les admins

--- vmvdc/vmvdc/routes/web.php
+++ vmvdc/vmvdc/routes/web.php
@@ -83,6 +83,7 @@ Route::get('sessionsA', 'listesAController@sessions')->name('sessionsA');
 Route::get('classesA', 'listesAController@classes')->name('classesA');
 Route::get('listeEnseignantsA', 'listesAController@enseignants')->name('listeEnseignantsA');
 Route::get('listeDoctorantsA', 'listesAController@doctorants')->name('listeDoctorantsA');
+Route::post('selectionClasse', 'listesAController@selectionClasse')->name('selectionClasse');
 
 Route::post("connexionA", "ConnexionControllerA@traitement")->name('connexionA');
 
