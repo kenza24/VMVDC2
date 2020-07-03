b72df50a9e0c26d5c4231627791e91c191534c2b
kenza24 <kenzazerouali00@gmail.com>
2020-07-03 13:04:32 +0200

    ajout de la table fichiers_sessions

---
+++ vmvdc/vmvdc/database/migrations/2020_07_03_103122_create_fichiers_sessions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFichiersSessionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fichiers_sessions', function (Blueprint $table) {
+            $table->integer('idSession');
+            $table->string('fichiers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fichiers_sessions');
+    }
+}
