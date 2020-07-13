f5ca19f58a4a26ae4ea8cfa0bffb03fbc3728e53
kenza24 <kenzazerouali00@gmail.com>
2020-07-13 11:15:36 +0200

    Update sessionsD.blade.php

--- vmvdc/vmvdc/resources/views/sessionsD.blade.php
+++ vmvdc/vmvdc/resources/views/sessionsD.blade.php
@@ -54,7 +54,7 @@
                 //dd($idS);?>
                   <!--si selectionner = 0, c'est que la session n'a pas été choisi encore -->
                     <td>
-                      <form method="post" action={{route('inscriptionD')}}  onsubmit="return confirm('Etes-vous sur ?');">
+                      <form method="post" action={{route('desinscriptionD')}}  onsubmit="return confirm('Etes-vous sur ?');">
                         {{csrf_field()}}
                         <input type="text" hidden name="idSession" value=<?= $session->id?>>
                         <button type="submit" class="btn btn-outline-danger">Se désinscrire</button>
