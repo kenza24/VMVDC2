f2de7ec3d3ec40ad1c56010922275cb3fe0b3e34
A-Evann <ambroisinevann@gmail.com>
2020-07-23 11:38:30 +0200

    tentative de responsivité de la page d'orientation de connexion

--- vmvdc/vmvdc/resources/views/orientationConnexion.blade.php
+++ vmvdc/vmvdc/resources/views/orientationConnexion.blade.php
@@ -1,26 +1,25 @@
 @extends('layout')
 @section('contenu')
 
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
-<div class="container-fluid">
-
 <div class="container" style="height:450px; font-family: Georgia, serif; ">
-
 
     <br>
     <hr>
 
     <div class="card bg-light" >
-        <article class="card-body mx-auto">
-            <h3>Connectez-vous en tant que:</h3>
-
-            <a href={{route('connexionE')}} class="btn btn-secondary row d-flex align-content-center flex-wrap mt-3 mb-3" role="button" aria-pressed="true">Enseignant de lycée </a>
-            <a href={{route('connexionD')}} class="btn btn-secondary row d-flex align-content-center flex-wrap mt-3 mb-3" role="button" aria-pressed="true">Doctorant</a>
-            <a href={{route('connexionA')}} class="btn btn-secondary row d-flex align-content-center flex-wrap mt-3 mb-3" role="button" aria-pressed="true">Administrateur</a>
+        <article class="card-body container d-flex flex-column flex-wrap">
+            <div class="d-flex justify-content-md-center text-break text-wrap flex-wrap">
+                <h3 class="d-flex flex-wrap">Connectez-vous en tant que:</h3>
+            </div>
+            <div class="d-flex justify-content-md-center text-break text-wrap flex-wrap">
+                <a href={{route('connexionE')}} class="btn btn-secondary d-flex flex-wrap mt-3 mb-3 text-break text-wrap col-12 px-md" role="button" aria-pressed="true" style="max-width: 20em">Enseignant de lycée </a>
+            </div> 
+            <div class="d-flex justify-content-md-center text-break text-wrap flex-wrap">
+                <a href={{route('connexionD')}} class="btn btn-secondary d-flex flex-wrap mt-3 mb-3 text-break text-wrap col-12 px-md" role="button" aria-pressed="true" style="max-width: 20em">Doctorant</a>
+            </div> 
+            <div class="d-flex justify-content-md-center text-break text-wrap flex-wrap">
+                <a href={{route('connexionA')}} class="btn btn-secondary d-flex flex-wrap mt-3 mb-3 text-break text-wrap col-12 px-md" role="button" aria-pressed="true" style="max-width: 20em">Administrateur</a>
+            </div> 
         </article>
     </div> <!-- card.// -->
 </div>
@@ -28,36 +27,5 @@
 
 <br><br>
 
-<style>
-.divider-text {
-    position: relative;
-    text-align: center;
-    margin-top: 15px;
-    margin-bottom: 15px;
-}
-.divider-text span {
-    padding: 7px;
-    font-size: 12px;
-    position: relative;
-    z-index: 2;
-}
-.divider-text:after {
-    content: "";
-    position: absolute;
-    width: 100%;
-    border-bottom: 1px solid #ddd;
-    top: 55%;
-    left: 0;
-    z-index: 1;
-}
-.btn-facebook {
-    background-color: #405D9D;
-    color: #fff;
-}
-.btn-twitter {
-    background-color: #42AEEC;
-    color: #fff;
-}
-</style>
 
 @endsection
